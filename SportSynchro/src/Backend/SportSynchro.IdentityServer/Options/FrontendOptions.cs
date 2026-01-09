179cb5f82a639ac7297d5ff8772ca6f891b447c0
RubenVD169 <rvd1027@proton.me>
2026-01-09 16:31:08 +0100

    removed some stuff that's unnecessary

--- SportSynchro/src/Backend/SportSynchro.IdentityServer/Options/FrontendOptions.cs
+++ SportSynchro/src/Backend/SportSynchro.IdentityServer/Options/FrontendOptions.cs
@@ -2,6 +2,5 @@ namespace SportSynchro.IdentityServer.Options;
 
 public sealed class FrontendOptions
 {
-    public string Authority { get; set; } = string.Empty;
     public string ClientUrl { get; set; } = string.Empty;
 }
